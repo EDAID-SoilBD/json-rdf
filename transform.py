189f888c20028dfbbcee01914567d7514567c520
Inés Díaz <inesdr6@uma.es>
2021-12-08 19:57:20 +0100

    New file and update

--- transform.py
+++ transform.py
@@ -13,192 +13,192 @@ obj = json.loads(data)
 
 
 g = Graph()
-result = g.parse("C:\Users\inesd\OneDrive\Escritorio\4º Ingeniería Bioinformática\Primer Cuatrimestre\Estándares de Datos Abiertos e Integración de Datos\Final Course Project\json-rdf\soilDB.owl")
+result = g.parse("C:\\Users\\inesd\\OneDrive\\Escritorio\\4º Ingeniería Bioinformática\\Primer Cuatrimestre\\Estándares de Datos Abiertos e Integración de Datos\\Final Course Project\\json-rdf\\soilDB.owl")
 base = 'http://www.semanticweb.org/inma/ontologies/2021/09/untitled-ontology-6'
 #type = g.value(semweb, RDFS.label)
 
 
 for row in obj:
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#CODE"),
         Literal(str(row['CODE']), datatype=XSD.string)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#PHOTOGRAPHS"),
         Literal(str(row['PHOTOGRAPHS']), datatype=XSD.string)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#DESCRIPTION"),
         Literal(str(row['DESCRIPTION']), datatype=XSD.string)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#COORDINATES X"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#COORDINATES_X"),
         Literal(str(row['COORDINATES X']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#COORDINATES Y"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#COORDINATES_Y"),
         Literal(str(row['COORDINATES Y']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#ALTITUDE"),
         Literal(str(row['ALTITUDE']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#INCLINE"),
         Literal(str(row['INCLINE']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#GRAVES"),
         Literal(str(row['GRAVES']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#VERY THICK SAND"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#VERY_THICK_SAND"),
         Literal(str(row['VERY THICK SAND']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#THICK SAND"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#THICK_SAND"),
         Literal(str(row['THICK SAND']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#MEDIUM SAND"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#MEDIUM_SAND"),
         Literal(str(row['MEDIUM SAND']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#FINE SAND"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#FINE_SAND"),
         Literal(str(row['FINE SAND']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#VERY FINE SAND"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#VERY_FINE_SAND"),
         Literal(str(row['VERY FINE SAND']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#TOTAL SAND"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#TOTAL_SAND"),
         Literal(str(row['TOTAL SAND']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#THICK LIMES"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#THICK_LIMES"),
         Literal(str(row['THICK LIMES']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#FINE LIMES"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#FINE_LIMES"),
         Literal(str(row['FINE LIMES']), datatype=XSD.int)
     ))
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#TOTAL LIMES"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#TOTAL_LIMES"),
         Literal(str(row['TOTAL LIMES']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#CLAY"),
         Literal(str(row['CLAY']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#K FACTOR"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#K_FACTOR"),
         Literal(str(row['K FACTOR']), datatype=XSD.double)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#APPARENT DENSITY"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#APPARENT_DENSITY"),
         Literal(str(row['APPARENT DENSITY']), datatype=XSD.double)
     ))
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#AGGREGATE STABILITY"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#AGGREGATE_STABILITY"),
         Literal(str(row['AGGREGATE STABILITY']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#PERMEABILITY"),
         Literal(str(row['PERMEABILITY']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#DESCRIPTION"),
         Literal(str(row['DESCRIPTION']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#FIELD CAPACITY"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#FIELD_CAPACITY"),
         Literal(str(row['FIELD CAPACITY']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#PERMANENT WILTING POINT"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#PERMANENT_WILTING_POINT"),
         Literal(str(row['PERMANENT WILTING POINT']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
         URIRef(base + "#HYDROPHOBICITY"),
         Literal(str(row['HYDROPHOBICITY']), datatype=XSD.int)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#ORGANIC CARBON"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#ORGANIC_CARBON"),
         Literal(str(row['ORGANIC CARBON']), datatype=XSD.string)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#C FACTOR"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#C_FACTOR"),
         Literal(str(row['C FACTOR']), datatype=XSD.double)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#ELECTRIC CONDUCTIVITY"),
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#ELECTRIC_CONDUCTIVITY"),
         Literal(str(row['ELECTRIC CONDUCTIVITY']), datatype=XSD.string)
     ))
 
     result.add((
-        URIRef(base + "#" + str(row['_id'])),
-        URIRef(base + "#SPECTRAL RESPONSE"),
-        Literal(str(row['SPECTRAL RESPONSE']), datatype=XSD.string)
+        URIRef(base + "#" + str(row['_id']['$oid'])),
+        URIRef(base + "#SPECTRAL_RESPONSE"),
+        Literal(str(row['SPECTRAL RESPONSE ']), datatype=XSD.string)
     ))
 
 
 f = open("PySoilDB.owl", "w")
-f.write(result.serialize(format="xml").decode())
+f.write(result.serialize(format="xml"))
 f.close()
 #tostring(catalogo, encoding='unicode')
